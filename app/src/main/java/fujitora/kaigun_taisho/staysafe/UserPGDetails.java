a3b78d85228bbd77e69b04abc4eca60fe04ab3a4
Ullas Diwakar <ullasdiwakar96@gmail.com>
2017-02-10 14:07:41 +0530

    Created Bed Details card.

---
+++ app/src/main/java/fujitora/kaigun_taisho/staysafe/UserPGDetails.java
@@ -0,0 +1,13 @@
+package fujitora.kaigun_taisho.staysafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserPGDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_pgdetails);
+    }
+}
